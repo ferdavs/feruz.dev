68c4595d931c45af8d1bb49268f57bae3c5b3f0e
Feruz <fdavletov@gmail.com>
2024-02-13 21:17:59 +0100

    embed CV

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,19 +1,22 @@
 <script>
+	import Body from './Body.svelte';
+
 	let VERSION = __APP_VERSION__;
 </script>
+
+<Body />
 
 <footer>
 	Version {VERSION}
 </footer>
 
 <style>
+	/* change position to bottom right */
 	footer {
 		position: fixed;
 		bottom: 0;
-		left: 0;
-		width: 100%;
-		background-color: #f8f9fa5c;
-		padding: 10px;
-		text-align: center;
+		right: 0;
+		padding: 1em;
+		background-color: #f0f0f076;
 	}
 </style>
---
+++ src/routes/CV.svelte
@@ -0,0 +1,12 @@
+<iframe
+	title="CV"
+	src="https://docs.google.com/document/d/e/2PACX-1vSCUSnRF-1F7XHej3fyW7RfkhIvyECfwK1eRfInqpV7htn2jCWI5pdCmJhI6O2yIxtz9A-3etyxjqwb/pub?embedded=true"
+></iframe>
+
+<style>
+	iframe {
+		width: 50%;
+		height: 300vh;
+		border: none;
+	}
+</style>
