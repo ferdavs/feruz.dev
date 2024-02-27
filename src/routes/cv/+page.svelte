b763e47ff48c043a20cc763d92b3e3cca566ca92
Feruz <fdavletov@gmail.com>
2024-02-27 22:22:17 +0100

    pdf embed

--- src/routes/cv/+page.svelte
+++ src/routes/cv/+page.svelte
@@ -1,14 +1,12 @@
-<iframe
-	title="CV"
-	src="https://docs.google.com/document/d/e/2PACX-1vSCUSnRF-1F7XHej3fyW7RfkhIvyECfwK1eRfInqpV7htn2jCWI5pdCmJhI6O2yIxtz9A-3etyxjqwb/pub?embedded=true"
-></iframe>
+<embed src="/cv.pdf" type="application/pdf" />
 
 <style>
-	iframe {
-		margin-top: -3.75em;
-		margin-left: -4.85em;
-		width: 150vh;
-		height: 350vh;
+	embed {
+		/* margin-top: -3.75em;
+		margin-left: -4.85em; */
+		/* width: 150vh;*/
+		height: 300vh;
 		border: none;
+		width: 100%;
 	}
 </style>
