95aa5366475f7e2a633eb076d17ae82ab8eaf4b6
Feruz <fdavletov@gmail.com>
2024-06-15 15:57:57 +0200

    cv: show cv image

--- src/routes/cv/+page.svelte
+++ src/routes/cv/+page.svelte
@@ -1,12 +1,34 @@
-<embed src="/cv.pdf" type="application/pdf" />
+<script>
+let files = __CV_FILES__.files;
+</script>
 
 <style>
-	embed {
-		/* margin-top: -3.75em;
-		margin-left: -4.85em; */
-		/* width: 150vh;*/
-		height: 300vh;
-		border: none;
-		width: 100%;
-	}
+:global(img) {
+  height: auto;
+  width: 800px;
+}
+
+.image-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  position: relative;
+}
+
+@media (min-width: 1600px) {
+  .image-container {
+    flex-direction: row;
+  }
+  .image-container img:nth-of-type(2) {
+    position: relative;
+    top: 110px; /* Offset the second image vertically */
+  }
+}
 </style>
+
+<div class="image-container">
+  {#each files as file, i}
+    <img src={file} alt="cv" />
+  {/each}
+</div>
--- vite.config.js
+++ vite.config.js
@@ -1,80 +1,55 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 import fs from 'fs';
-import { exec, execSync } from 'child_process';
+import { execSync, spawnSync } from 'child_process';
+
+const source = './rendercv_output';
+const destination = './static';
+
+function listFiles(folder, ending) {
+	return fs.readdirSync(folder)
+		.filter(file => file.endsWith(ending)).sort();
+}
+
+function listCVFiles(folder, prefix = 'cv') {
+	return listFiles(folder, '.png')
+		.filter(file => file.startsWith(prefix)).sort();
+}
+
 function renderCV() {
 	console.log('Rendering CV');
-	// skip if nothing has changed
-	let files = fs.readdirSync('.').filter(file => file.endsWith("CV.yaml"));
-	let lastModified = Math.max(...files.map(file => fs.statSync(file).mtime));
-	let cv = 'static/cv.pdf';
-	if (fs.existsSync(cv)) {
-		let cvModified = fs.statSync(cv).mtime;
-		if (cvModified > lastModified) {
-			return { path: '/cv.pdf' };
-		}
-	}
-	exec('poetry run rendercv render *CV.yaml', (err, stdout, stderr) => {
-		if (err) {
-			console.error(stderr);
-			return;
-		}
-		console.log(stdout);
-		const source = 'rendercv_output';
-		const destination = 'static';
-		let files = fs.readdirSync(source);
-		let cv = files.find(file => file.endsWith('CV.pdf'));
-		if (cv) {
-			fs.copyFileSync(`${source}/${cv}`, `${destination}/cv.pdf`);
-		}
-		let readme = files.find(file => file.endsWith('CV.md'));
-		if (readme) {
-			fs.copyFileSync(`${source}/${readme}`, `README.md`);
-		}
 
-		let tex = files.find(file => file.endsWith('CV.tex'));
-		if (tex) {
-			// execSync(`pandoc -f latex -t html -o static/cv.html ./${source}/${tex}`);
-		}
+	listCVFiles(source, "Feruz")
+		.forEach(file => fs.unlinkSync(`${source}/${file}`));
+	
+	listCVFiles(destination)
+		.forEach(file => fs.unlinkSync(`${destination}/${file}`));
 
-		// files.forEach(file => {
-		// 	fs.unlinkSync(`${source}/${file}`);
-		// });
-	});
-	return { path: '/cv.pdf' };
-}
-
-function convertNotebooks() {
-	console.log('Converting Jupyter notebooks');
-	// skip if nothing has changed
-	let files = fs.readdirSync('projects').filter(file => file.endsWith('.ipynb'));
-	let lastModified = Math.max(...files.map(file => fs.statSync(`projects/${file}`).mtime));
-	let lastConverted = Math.max(...files.map(file => fs.statSync(`src/routes/projects/p/${file.replace('.ipynb', '.svelte')}`).mtime));
-	if (lastModified < lastConverted) {
-		return files.map(file => file.replace('.ipynb', ''));
-	}
-	// list of ipynb files
-	const notebooks = fs.readdirSync('projects').filter(file => file.endsWith('.ipynb'));
-	// convert to svelte
-	notebooks.forEach(notebook => {
-		execSync(`jupyter2svelte convert --embed-images projects/${notebook}`);
-		let component = notebook.replace('.ipynb', '.svelte');
-		fs.renameSync(`projects/${component}`, `src/routes/projects/p/${component}`);
-	});
-	console.log('Converted', notebooks);
-	return notebooks.map(notebook => notebook.replace('.ipynb', ''));
-}
-
-function listPdf() {
-	return fs.readdirSync('static').filter(file => file.endsWith('.pdf') && file !== 'cv.pdf');
+	execSync('rendercv render *CV.yaml');
+	// copy only png files
+	listCVFiles(source, "Feruz")
+			.forEach(file => {
+				console.log(file);
+				// copy files with names cv1.png, cv2.png, etc.
+				let match = file.match(/(\d+)/);
+				let number = match ? match[1] : 1;
+				fs.copyFileSync(`${source}/${file}`,
+					`${destination}/cv${number}.png`);
+			});
+	
+	let readme = listFiles(source, '.md').find(file => file.endsWith('CV.md'));
+	if (readme) 
+		fs.copyFileSync(`${source}/${readme}`, 'README.md');
+	
+	return {
+		files: listCVFiles(destination),
+	};
 }
 
 export default defineConfig({
 	plugins: [sveltekit()],
 	define: {
-		__CV_PATH__: renderCV(),
-		// __NOTEBOOKS__: convertNotebooks(),
-		__PDFS__: listPdf(),
+		__CV_FILES__: renderCV(),
 		__APP_VERSION__: JSON.stringify(require('./package.json').version),
 	}
 });
